78032008ff6ef785c97cbc239eb9ccadde3be37a
AbhiVarde <91776428+AbhiVarde@users.noreply.github.com>
2023-06-01 15:19:34 +0530

    initial commit

--- src/components/UserProfile.tsx
+++ src/components/UserProfile.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, Link } from "react-router-dom";
 import axios from "axios";
 
 interface User {
@@ -42,20 +42,28 @@ function UserProfile() {
   }
 
   return (
-    <div className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl mx-auto bg-black rounded-lg shadow-md overflow-hidden">
-      <img
-        className="w-full h-auto sm:h-48 md:h-56 lg:h-64 object-cover object-center max-w-full"
-        src={user.avatar_url}
-        alt={user.login}
-      />
-      <div className="p-4 text-white text-center">
-        <h2 className="text-[#FFD700] text-xl font-bold mb-2">{user.login}</h2>
-        <p className="text-slate-400">{user.bio}</p>
-        <p className="mt-4">Followers: {user.followers}</p>
-        <p>Following: {user.following}</p>
-        <p>Public Repos: {user.public_repos}</p>
+    <Link
+      to={`https://github.com/${user.login}`}
+      target="_blank"
+      rel="noopener noreferrer"
+    >
+      <div className="max-w-xs sm:max-w-sm md:max-w-md lg:max-w-lg xl:max-w-xl 2xl:max-w-2xl mx-auto bg-black rounded-lg shadow-md overflow-hidden">
+        <img
+          className="w-full h-auto sm:h-48 md:h-56 lg:h-64 object-cover object-center max-w-full"
+          src={user.avatar_url}
+          alt={user.login}
+        />
+        <div className="p-4 text-white text-center">
+          <h2 className="text-[#FFD700] text-xl font-bold mb-2">
+            {user.login}
+          </h2>
+          <p className="text-slate-400">{user.bio}</p>
+          <p className="mt-4">Followers: {user.followers}</p>
+          <p>Following: {user.following}</p>
+          <p>Public Repos: {user.public_repos}</p>
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
